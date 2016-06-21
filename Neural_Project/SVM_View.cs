623b09e1652635fbbff8b19f72dd3c8d5e02f5c4
micheayoussef <michealysha@yahoo.com>
2016-06-21 17:43:41 +0200

    final version

---
+++ Neural_Project/SVM_View.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Neural_Project
+{
+    public partial class SVM_View : Form
+    {
+        SVM_handler SH = null;
+        public SVM_View(ReadImages RM)
+        {
+            InitializeComponent();
+            SH = new SVM_handler(RM, this.dataGridView_confusion_matrix, this.textBox_overall_accuracy, this.Trainingtime, this.Testing_time, label1, label2);
+        }
+        public SVM_View()
+        {
+            InitializeComponent(); 
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SH.openIMage();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SVM_View_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
